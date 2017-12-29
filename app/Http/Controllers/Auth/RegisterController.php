18389811b4ad6495f3dee26f79213effd603b902
leo851001 <leohuang1996@gmail.com>
2017-12-30 06:43:00 +0800

    debug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,8 +48,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'login_name' => 'required|string|max:12',
+            'user_name' => 'required|string|max:12',
+            'email' => 'required|string|email|max:255|',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
@@ -63,7 +64,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'login_name' => $data['login_name'],
+            'user_name' => $data['user_name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
