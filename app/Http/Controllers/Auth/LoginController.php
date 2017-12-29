b248603ff9c9493527e67ac52ca37cc2bfa4838f
leo851001 <leohuang1996@gmail.com>
2017-12-30 06:45:41 +0800

    新增一般會員停權功能

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,6 +1,7 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
+use Illuminate\Http\Request;
+use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 class LoginController extends Controller
@@ -39,5 +40,22 @@ class LoginController extends Controller
     public function username()
     {
         return 'login_name';
+    }
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        $active_user = User::where($this->username(), $request[$this->username()])
+            ->where('status', User::STATUS_ACTIVE)->first();
+        if ($active_user !== null) {
+            return $this->guard()->attempt(
+                $this->credentials($request), $request->has('remember')
+            );
+        }
+        return false;
     }
 }
